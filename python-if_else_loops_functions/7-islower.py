bab8c30688b80c1f80dd41d26e019a1356dba5b0
TurkanBaghirzade✮⋆˙ <137319134+Prenses123@users.noreply.github.com>
2026-03-21 23:20:16 +0400

    Update 7-islower.py

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     i = ord(c)
-    if 96 < c < 123:
+    if 97 <= c <= 122:
         print("{} is lower".format(c))
         return True
     print("{} is upper".format(c))
