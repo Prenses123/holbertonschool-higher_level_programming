2fdc39ebef82c0f33922bb1a26d2525f4dbf95be
TurkanBaghirzade✮⋆˙ <137319134+Prenses123@users.noreply.github.com>
2026-03-22 16:19:47 +0400

    Update 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from sys import argv
 
-for i in range(1, len(argv)):
-    print("{}: {}".format(i, argv[i]))
+if __name__ == "__main__":
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
