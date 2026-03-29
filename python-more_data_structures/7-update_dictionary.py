692f9edabb1bed39591c5a65d21fb6f8d1fa846c
TurkanBaghirzade✮⋆˙ <137319134+Prenses123@users.noreply.github.com>
2026-03-29 14:52:09 +0400

    Update 7-update_dictionary.py

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     a_dictionary.update({key: value})
-    for i in a_dictionary.keys():
-        print(f"{i}: {a_dictionary[i]}")
