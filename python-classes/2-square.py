e9b51620a475ffc11fae65e14c157f69e10d91b3
TurkanBaghirzade✮⋆˙ <137319134+Prenses123@users.noreply.github.com>
2026-04-04 19:22:21 +0400

    Update 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,11 +5,12 @@
 class Square:
     """Defines a square with a size attribute."""
 
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """Initializes a square with a given size."""
-        if type(size) != int:
+
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
+
         if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
