33218c34c9d12764841ebcad18be222c20d5f8d6
Your Name <you@example.com>
2026-04-04 15:10:18 +0000

    add

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This is an empty Square class."""
+
+
+class Square:
+    """Defines a square with a size attribute."""
+
+    def __init__(self, size):
+        """Initializes a square with a given size."""
+        self.__size = size
