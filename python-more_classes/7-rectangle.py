c770da71792535d5e6560d73860ae96d6aaeb813
TurkanBaghirzade✮⋆˙ <137319134+Prenses123@users.noreply.github.com>
2026-04-05 23:58:46 +0400

    Update 7-rectangle.py

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""salam"""
+"""hsagdjhgsdja"""
 
 
 class Rectangle:
-    "salam"
+    "sdfsdfsd"
     number_of_instances = 0
     print_symbol = "#"
 
@@ -38,11 +38,11 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        "salam"
+        "jhkg"
         return self.__width * self.__height
 
     def perimeter(self):
-        "salam"
+        "hgjgjg"
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
@@ -73,3 +73,17 @@ class Rectangle:
     def __del__(self):
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() == rect_2.area():
+            return rect_1
+        elif rect_1.area() > rect_2.area():
+            return rect_1
+        else:
+            return rect_2
+    
