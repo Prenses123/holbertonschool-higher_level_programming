ff38b6fb2c7cd4358ab2e0a53eb3336442c0cd86
Your Name <you@example.com>
2026-04-08 20:16:37 +0000

    add

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This is an empty Square class."""
+
+
+class MyList(self, list):
+    list = []
+    def __init__(self, list):
+        self.list = list
+    def print_sorted(self):
+        return sorted(self.list)
