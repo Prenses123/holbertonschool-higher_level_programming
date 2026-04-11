a3a7c81426da825ef88002ff04201f0d1bb9d273
TurkanBaghirzade✮⋆˙ <137319134+Prenses123@users.noreply.github.com>
2026-04-12 00:12:15 +0400

    Update 7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -6,9 +6,9 @@ class BaseGeometry:
     """jdjdj"""
     def integer_validator(self, name, value):
         if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(self.name))
+            raise TypeError("name must be an integer".format())
         if self.value <= 0:
-            raise ValueError("{} must be greater than 0".format(self.name))
+            raise ValueError("name must be greater than 0".format())
         self.name = name
         self.value = value
     def area(self):
