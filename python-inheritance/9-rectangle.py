eeec9c8e4adfff3e4b8fbed600cd541c362d13af
Your Name <you@example.com>
2026-04-12 15:21:26 +0000

    add

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"hjdsfkhd"
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    "dgdfgd"
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area():
+        return self.__height * self.__width
+
+    def print():
+        print(f"[Rectangle] {width}/{height}")
+
+    def __str__(self):
+        return f"[Rectangle] {width}/{height}"
