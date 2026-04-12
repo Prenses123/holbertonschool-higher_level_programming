65405611f6d230e2d9c10ad989401f41b47c142c
Your Name <you@example.com>
2026-04-12 16:19:33 +0000

    add task file

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"my module"
+from abc import ABC
+
+
+class Animal(ABC):
+    @abstractmethod
+    def sound():
+        pass
+class Dog(Animal):
+    def sound():
+        print(f"Bark")
+class Cat(Animal):
+    def sound():
+        print(f"Meow")
