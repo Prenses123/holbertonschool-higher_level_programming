56ceaa6fff05f10c1d536404581f1b34ba73d514
Your Name <you@example.com>
2026-04-12 14:55:01 +0000

    add

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""MyList module"""
+
+
+class BaseGeometry:
+    """jdjdj"""
+    def area(self):
+        raise Exception("area() is not implemented".format())
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+class Rectangle(BaseGeometry):
+    """hdhfh"""
+    def __init__(self, width, height):
+        def integer_validator(self, width, height):
+            if width < 0 or height < 0:
+                return False
+    self.__width = width
+    self.__height = height
