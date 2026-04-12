aacd82f3fcb6238a0f50177aef944714cab8819f
Your Name <you@example.com>
2026-04-12 17:16:06 +0000

    add

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"my module"
+from abc import ABC
+import math
+
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+    def perimeter(self):
+        pass
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        return math.pi * self.radius ** 2
+    def perimeter():
+        return 2 * math.pi * self.radius
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    def area(self):
+        return self.height * self.width
+    def perimeter():
+        return 2 * (self.height + self.width)
+def shape_info(shape):
+    print("Area:", shape.area())
+    print("Perimeter:", shape.perimeter())
