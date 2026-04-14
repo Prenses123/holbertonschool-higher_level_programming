21197a1bb8d88c27c24cc3834e08073f9f68a7bb
TurkanBaghirzade✮⋆˙ <137319134+Prenses123@users.noreply.github.com>
2026-04-14 12:24:21 +0400

    Update 1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,5 +5,5 @@
 def write_file(filename="", text=""):
     """add"""
     with open(filename, "w+", encoding="utf-8") as f:
-        f.write(text, end="\n")
+        f.write(text)
         print(f.read())
