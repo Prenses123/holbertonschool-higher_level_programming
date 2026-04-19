aebcfa81c28accf5636832eefa88f39814b405a7
Your Name <you@example.com>
2026-04-19 15:29:55 +0000

    add

---
+++ python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"sdfsdfds"
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list):
+            new_dict = {}
+            for i in attrs:
+                if isinstance(i, str) and i in self.__dict__:
+                    new_dict[i] = self.__dict__[i]
+            return new_dict
+        else:
+            self.__dict__
