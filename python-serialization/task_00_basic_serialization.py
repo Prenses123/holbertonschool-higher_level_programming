00684e9faa5e60ba410b201270be32556086c3e6
Your Name <you@example.com>
2026-04-26 11:15:31 +0000

    add

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"json to dict"
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    with open(filename, "w", encoding="utf-8") as file:
+        return json.dump(data, file)
+
+def load_and_deserialize(filename):
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
