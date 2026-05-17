cf37cd8cf5f6764d5c0b6b4b55bf00ec6286dbd8
TurkanBaghirzade✮⋆˙ <137319134+Prenses123@users.noreply.github.com>
2026-05-18 00:37:42 +0400

    Update 2-create_read_user.sql

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,12 +1,12 @@
--- dskfhds
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-
-GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
-
+-- create database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
+-- create user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
 
+-- grant privileges
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- apply changes
+FLUSH PRIVILEGES;
