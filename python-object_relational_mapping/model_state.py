ef51973002bff100021b03178458a4a225095287
TurkanBaghirzade✮⋆˙ <137319134+Prenses123@users.noreply.github.com>
2026-05-20 16:40:37 +0400

    Update model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-"""Module that defines a State class"""
+"vsdfsdfds"
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
 
 Base = declarative_base()
+engine = create_engine("sqlite:///states.db")
+
+Base.metadata.create_all(engine)
 
 
 class State(Base):
-    """State class that maps to the states table"""
-
+    "sdfsdfsd"
     __tablename__ = 'states'
-
-    id = Column(Integer, primary_key=True, nullable=False,
-                autoincrement=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
+    cities = relationship("City", back_populates="state")
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
