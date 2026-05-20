c8e86abed1582d3790de76dbbfaaac4a53ad3f25
TurkanBaghirzade✮⋆˙ <137319134+Prenses123@users.noreply.github.com>
2026-05-20 16:39:25 +0400

    Refactor city fetch logic to use filter method

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,31 +1,29 @@
 #!/usr/bin/python3
-"""Lists all State objects from a MySQL database"""
-
+"""Lists all City objects with their State name"""
 
 import sys
-from model_state import State, Base
-from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+from model_state import Base, State
+from model_city import City
 
+if __name__ == "__main__":
     engine = create_engine(
-        f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}",
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        ),
         pool_pre_ping=True
-        )
+    )
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    results = (session.query(State, City)
-               .join(City, City.state_id == State.id)
-               .order_by(City.id).all())
-    for state, city in results:
-        print(f"{state.name}: ({city.id}) {city.name}")
+    results = session.query(State, City).filter(
+        State.id == City.state_id
+    ).order_by(City.id).all()
 
-    session.commit()
-    session.close()
+    for state, city in results:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
